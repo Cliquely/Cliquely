f27b88e04a999bdc9100044ab8d2175957618389
Noam Chapnik <chapnik13@gmail.com>
2018-11-23 15:17:13 +0200

    + Added local website to render the network + Added UserControl that is able to show the render the site (only works on x86)

---
+++ BacteriaNetworks/NetworkViewer.cs
@@ -0,0 +1,33 @@
+﻿using CefSharp.WinForms;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BacteriaNetworks
+{
+    public partial class NetworkViewer : UserControl
+    {
+        private ChromiumWebBrowser browser;
+
+        public NetworkViewer(List<Gene> genes, List<List<Gene>> cliques)
+        {
+            InitializeComponent();
+            var jsonGenes = JsonConvert.SerializeObject(genes.Select(gene => gene.Id));
+            var jsonCliques = JsonConvert.SerializeObject(cliques.Select(clique => clique.Select(gene => gene.Id)));
+            var address = $"file:///{Directory.GetCurrentDirectory()}/NetworkVisualization/index.html?genes={jsonGenes}&cliques={jsonCliques}";
+
+            initializeWebBrowser(address);
+        }
+
+        private void initializeWebBrowser(string address)
+        {
+            browser = new ChromiumWebBrowser(address);
+            browser.Dock = DockStyle.Fill;
+            browser.Location = new System.Drawing.Point(0, 0);
+            browser.TabIndex = 0;
+            Controls.Add(browser);
+        }
+    }
+}
--- BacteriaNetworks/Program.cs
+++ BacteriaNetworks/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace BacteriaNetworks
+﻿using System.Collections.Generic;
+
+namespace BacteriaNetworks
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var g1 = new Gene(1, null);
+            var g2 = new Gene(2, null);
+            var g3 = new Gene(3, null);
+            var g4 = new Gene(4, null);
+
+            var clique1 = new List<Gene>() { g1, g2, g3 };
+            var clique2 = new List<Gene>() { g1, g2, g4 };
+
+            NetworkVisualization s = new NetworkVisualization(new List<Gene>() { g1, g2, g3, g4 }, new List<List<Gene>>() { clique1, clique2 });
+            s.ShowDialog();
         }
     }
 }
