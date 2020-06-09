e912e2469fa0d304f3375a6adbdd50a1b60b0be6
Noam Chapnik <chapnik13@gmail.com>
2020-06-09 19:04:25 +0300

    Added some stuff to export a genome file with a lot of information

---
+++ GenomsFullInfo/Genome.cs
@@ -0,0 +1,27 @@
+﻿namespace GenomsFullInfo
+{
+    public class Genome
+    {
+        public string Abbreviation { get; set; }
+        public string FullName { get; set; }
+        public string Taxonomy { get; set; }
+        public int NumberOfGenes { get; set; }
+        public int NumberOfClusters { get; set; }
+        public int NumberOfCliques { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Abbreviation.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Genome genome)
+            {
+                return genome.Abbreviation == Abbreviation;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ GenomsFullInfo/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GenomsFullInfo
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var genomes = await GetGenomes(@"data\genome_line.txt");
+            await GetGenomesNumberOfGenes(genomes, @"data\cleaned_data_reverse.txt");
+            await GetGenomesNumberOfCliques(genomes, @"data\Cliques100%.csv");
+            await GetGenomesNumberOfClusters(genomes, @"C:\Users\NoamChapnik\Desktop\mbgd_2016-01_extended.tab");
+
+            await WriteToCsv(genomes, "Genomes Details.csv");
+        }
+
+        private static async Task WriteToCsv(Dictionary<string, Genome> genomes, string filename)
+        {
+            using var writer = new StreamWriter(filename);
+
+            await writer.WriteLineAsync("Abbreviation, FullName, Taxonomy, NumberOfGenes, NumberOfClusters, NumberOfCliques");
+
+            foreach(var genome in genomes.Values)
+            {
+                await writer.WriteLineAsync($"{genome.Abbreviation}, {genome.FullName}, {genome.Taxonomy}, {genome.NumberOfGenes}, {genome.NumberOfClusters}, {genome.NumberOfCliques}");
+            }
+        }
+
+        private static async Task<Dictionary<string, Genome>> GetGenomes(string filename)
+        {
+            var genomes = new Dictionary<string, Genome>(4742);
+            var genomesReader = DataFilesReader.ReadFile(filename);
+
+            //taxnomy   Abbreviation fullname
+            await foreach (var genome in genomesReader)
+            {
+                genomes.Add(genome[1], new Genome { Taxonomy = genome[0], Abbreviation = genome[1], FullName = genome[2] });
+            }
+
+            return genomes;
+        }
+
+        private static async Task GetGenomesNumberOfClusters(Dictionary<string, Genome> genomes, string reversedDataFileName)
+        {
+            var reverseDataReader = DataFilesReader.ReadFile(reversedDataFileName, 2);
+
+            //Abbreviation  count   list
+            await foreach(var genome in reverseDataReader)
+            {
+                var abbreviation = genome[0];
+
+                if (genomes.ContainsKey(abbreviation))
+                {
+                    genomes[abbreviation].NumberOfClusters = int.Parse(genome[1]);
+                }
+            }
+        }
+
+        private static async Task GetGenomesNumberOfCliques(Dictionary<string, Genome> genomes, string cliquesFileName)
+        {
+            var cliquesReader = DataFilesReader.ReadFile(cliquesFileName, 1, ',');
+
+            //Taxonomy  fullName    Abbreviation    AmountOfGenes   GenesList
+            await foreach (var clique in cliquesReader)
+            {
+                var abbreviation = clique[2];
+
+                if (genomes.ContainsKey(abbreviation))
+                {
+                    genomes[abbreviation].NumberOfCliques++;
+                }
+            }
+        }
+
+        private static async Task GetGenomesNumberOfGenes(Dictionary<string, Genome> genomes, string clustersFileName)
+        {
+            var clustersReader = DataFilesReader.ReadFile(clustersFileName, 9, splitOptions: StringSplitOptions.None);
+
+            await foreach (var row in clustersReader)
+            {
+                var genes = row[8..];
+
+                foreach (var gene in genes)
+                {
+                    if (!string.IsNullOrEmpty(gene))
+                    {
+                        var abbreviation = gene.Substring(0, gene.IndexOf(':'));
+                        var amount = gene.Split(' ', StringSplitOptions.RemoveEmptyEntries).Length;
+
+                        if (genomes.ContainsKey(abbreviation))
+                        {
+                            genomes[abbreviation].NumberOfGenes += amount;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
